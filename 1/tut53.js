4cf284e31ab89d0474e82a3753b6aa5b5737907c
Khnzz07 <110941221+Khnzz07@users.noreply.github.com>
2023-08-04 12:07:09 +0500

    Add files via upload

---
+++ 1/tut53.js
@@ -0,0 +1,21 @@
+console.log("This is tutorial 53");
+
+function greet(name,text){
+    console.log(text + " " + name )
+}
+let name = "Farhan";
+let name1 = "Abdul";
+let name2 = "Fari";
+let name3 = "Awais";
+let text = "Goood Morning"
+greet(name,text);
+greet(name1,text);
+greet(name2,text);
+greet(name3,text);
+
+function sum(a,b,c){
+    d = a+b+c;
+    return d;
+}
+let returnval = sum(4,6,4);
+console.log(returnval);
